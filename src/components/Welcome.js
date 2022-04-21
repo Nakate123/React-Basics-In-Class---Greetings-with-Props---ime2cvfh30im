d20259f20920561983c4d38233e1d8fd11f4cd38
Nakate123 <94731837+Nakate123@users.noreply.github.com>
2022-04-21 07:44:23 +0530

    New Changes Done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,8 +3,8 @@ function welcome(props) {
   const name = props.name;
   return (
     <div>
-      <h1>Hey!{name}</h1>
-      <h2> Welcome to Newton School.</h2>
+      <h1>Hey !{name}</h1>
+      <h2>Welcome to Newton School.</h2>
     </div>
   );
 }
