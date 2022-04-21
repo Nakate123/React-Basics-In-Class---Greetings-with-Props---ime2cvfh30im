76d85117a7dcf812355a46074e746ac93cf80571
Nakate123 <94731837+Nakate123@users.noreply.github.com>
2022-04-21 07:31:49 +0530

    ass Assi done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
-// write code for Welcome component here
+import React from "react";
+function welcome(props) {
+  const name = props.name;
+  return (
+    <div>
+      <h1>Hey {name}</h1>
+      <h2> Welcome to Newton School.</h2>
+    </div>
+  );
+}
+export default welcome;
